979932f3802ea1ac92be363002e2b0f685b8c27e
Parth Chokhra <35425925+parthplc@users.noreply.github.com>
2020-08-25 21:11:35 +0530

    Add files via upload

--- questgen.py
+++ questgen.py
@@ -15,6 +15,7 @@ from collections import OrderedDict
 import string
 import pke
 import nltk
+import numpy 
 from nltk import FreqDist
 nltk.download('brown')
 nltk.download('stopwords')
@@ -30,14 +31,14 @@ from mcq import get_keywords
 from mcq import get_sentences_for_keyword
 from mcq import generate_questions_mcq
 from mcq import generate_normal_questions
-
+import time
 class PythonPredictor:
     
     def __init__(self):
         model_file_1 = "../input/s2v-old/s2v_old"
         
         self.tokenizer = T5Tokenizer.from_pretrained('t5-base')
-        model = T5ForConditionalGeneration.from_pretrained('../input/multiquestiongenerator/result')
+        model = T5ForConditionalGeneration.from_pretrained('Parth/result')
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
         # model.eval()
@@ -141,38 +142,6 @@ class PythonPredictor:
 
         return final_output
             
-    def random_choice(self):
-        a = random.choice([0,1])
-        return bool(a)
-    
-    def predict_boolq(self,payload):
-        start = time.time()
-        inp = {
-            "input_text": payload.get("input_text"),
-            "max_questions": payload.get("max_questions", 4)
-        }
-
-        text = inp['input_text']
-        num= inp['max_questions']
-        sentences = tokenize_sentences(text)
-        joiner = " "
-        modified_text = joiner.join(sentences)
-        answer = self.random_choice()
-        form = "truefalse: %s passage: %s </s>" % (modified_text, answer)
-
-        encoding = self.tokenizer.encode_plus(form, return_tensors="pt")
-        input_ids, attention_masks = encoding["input_ids"].to(self.device), encoding["attention_mask"].to(self.device)
-
-        output = beam_search_decoding (input_ids, attention_masks,self.model,self.tokenizer)
-        if torch.device=='cuda':
-            torch.cuda.empty_cache()
-        
-        final= {}
-        final['Text']= text
-        final['Count']= num
-        final['Boolean Questions']= output
-            
-        return final
   
     def paraphrase(self,payload):
         start = time.time()
@@ -221,4 +190,97 @@ class PythonPredictor:
         if torch.device=='cuda':
             torch.cuda.empty_cache()
         
+        return output
+
+
+class BooleanPredictor:
+       
+    def __init__(self):
+        self.tokenizer = T5Tokenizer.from_pretrained('t5-base')
+        model = T5ForConditionalGeneration.from_pretrained('ramsrigouthamg/t5_boolean_questions')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model.to(device)
+        # model.eval()
+        self.device = device
+        self.model = model
+        self.set_seed(42)
+        
+    def set_seed(self,seed):
+        numpy.random.seed(seed)
+        torch.manual_seed(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
+
+    def random_choice(self):
+        a = random.choice([0,1])
+        return bool(a)
+    
+
+    def predict_boolq(self,payload):
+        start = time.time()
+        inp = {
+            "input_text": payload.get("input_text"),
+            "max_questions": payload.get("max_questions", 4)
+        }
+
+        text = inp['input_text']
+        num= inp['max_questions']
+        sentences = tokenize_sentences(text)
+        joiner = " "
+        modified_text = joiner.join(sentences)
+        answer = self.random_choice()
+        form = "truefalse: %s passage: %s </s>" % (modified_text, answer)
+
+        encoding = self.tokenizer.encode_plus(form, return_tensors="pt")
+        input_ids, attention_masks = encoding["input_ids"].to(self.device), encoding["attention_mask"].to(self.device)
+
+        output = beam_search_decoding (input_ids, attention_masks,self.model,self.tokenizer)
+        if torch.device=='cuda':
+            torch.cuda.empty_cache()
+        
+        final= {}
+        final['Text']= text
+        final['Count']= num
+        final['Boolean Questions']= output
+            
+        return final
+            
+class AnswerPredictor:
+          
+    def __init__(self):
+        self.tokenizer = T5Tokenizer.from_pretrained('t5-base')
+        model = T5ForConditionalGeneration.from_pretrained('Parth/boolean')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model.to(device)
+        # model.eval()
+        self.device = device
+        self.model = model
+        self.set_seed(42)
+        
+    def set_seed(self,seed):
+        numpy.random.seed(seed)
+        torch.manual_seed(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
+
+    def greedy_decoding (inp_ids,attn_mask):
+        greedy_output = model.generate(input_ids=inp_ids, attention_mask=attn_mask, max_length=256)
+        Question =  tokenizer.decode(greedy_output[0], skip_special_tokens=True,clean_up_tokenization_spaces=True)
+        return Question.strip().capitalize()
+
+    def predict_answer(self,payload):
+        start = time.time()
+        inp = {
+            "input_text": payload.get("input_text"),
+            "input_question" : payload.get("input_question")
+        }
+
+        context = inp["input_text"]
+        question = inp["input_question"]
+        input = "question: %s <s> context: %s </s>" % (question,context)
+
+        encoding = tokenizer.encode_plus(input, return_tensors="pt")
+        input_ids, attention_masks = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
+        output = greedy_decoding(input_ids,attention_masks)
+
         return output
