c00f52007b2dd6ac7732e245c13ed2a070ce6d48
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-05-04 19:08:23 +0900

    feat: update library

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(name='Questgen',
       url="https://github.com/ramsrigouthamg/Questgen.ai",
       install_requires=[
          
-           'torch==1.5.1',
+           'torch==1.8.1',
            'transformers==3.0.2',
            'pytorch_lightning==0.8.1',
-           'sense2vec==1.0.2',
+           'sense2vec==1.0.3',
            'strsim==0.0.3',
            'six==1.12.0',
            'networkx==2.4.0',
@@ -27,6 +27,7 @@ setup(name='Questgen',
            'python-dateutil==2.8.1',
            'boto3==1.14.40',
            'flashtext==2.7'
+           'pandas'
       ],
       package_data={'Questgen': ['questgen.py', 'mcq.py', 'train_gpu.py', 'encoding.py']}
       )
