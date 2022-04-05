032eb0b2a5e2bdadc808b6b12c78d67e2b66998e
Ramsri Goutham Golla <ramsrigouthamg@gmail.com>
2022-04-05 22:02:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,22 +9,22 @@ setup(name='Questgen',
       url="https://github.com/ramsrigouthamg/Questgen.ai",
       install_requires=[
          
-           'torch==1.8.1',
+           'torch==1.10.0',
            'transformers==3.0.2',
            'sense2vec==2.0.0',
-           'spacy==3.2.4',
            'strsim==0.0.3',
-           'flashtext==2.7',
            'six==1.15.0',
-           'networkx==2.4.0',
-           'numpy==1.21.0',
+           'networkx==2.6.3',
+           'numpy==1.21.5',
            'scipy==1.4.1',
-           'scikit-learn==0.22.1',
-           'unidecode==1.1.1',
-           'future==0.18.2',
-           'joblib==0.14.1',
-           'python-dateutil==2.8.1',
-           'pandas==1.1.1'
+           'scikit-learn==1.0.2',
+           'unidecode==1.3.4',
+           'future==0.16.0',
+           'joblib==1.1.0',
+           'pytz==2018.9',
+           'python-dateutil==2.8.2',
+           'flashtext==2.7',
+           'pandas==1.3.5'
       ],
       package_data={'Questgen': ['questgen.py', 'mcq.py', 'train_gpu.py', 'encoding.py']}
       )
