5564e51f8ae5cec09a4a91430f2d020ca39d0cc3
pooya Games <426713+pooyaEst@users.noreply.github.com>
2023-06-01 22:14:31 +0330

    Update setup.py
    upgrade package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Questgen',
-      version='1.0.0',
+      version='1.0.1',
       description='Question generator from any text',
       author='Questgen contributors',
       author_email='vaibhavtiwarifu@gmail.com',
